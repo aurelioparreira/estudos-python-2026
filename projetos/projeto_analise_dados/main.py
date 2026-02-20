031fb29418b9b5f20e57ab50faf3dc736cbea806
Aurelio José Parreira <profaurelioparreira@gmail.com>
2026-02-20 10:40:19 -0300

    docs: adiciona projeto de analise de dados

---
+++ projetos/projeto_analise_dados/main.py
@@ -0,0 +1,12 @@
+## 📊 Projeto: Análise de Dados Simples
+
+Projeto desenvolvido em Python para análise básica de dados.
+
+Funcionalidades:
+
+- Entrada de dados
+- Cálculo de média
+- Identificação de maior e menor valor
+
+Arquivo:
+projetos/projeto_analise_dados/main.py
